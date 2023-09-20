465c4c3bc443aadcd29446619f02f7dd867d077d
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-19 19:07:01 -0700

    Update d1y-p4r72.sh

--- d1y-p4r72.sh
+++ d1y-p4r72.sh
@@ -28,31 +28,31 @@ sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_
 sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
 
 # install turboacc and patch
-echo "install turboacc and patch for nftables"
-ls
-mkdir -p turboacc_tmp ./package/turboacc
-cd turboacc_tmp
-git clone https://github.com/chenmozhijin/turboacc -b package
-cd ../package/turboacc
-git clone https://github.com/fullcone-nat-nftables/nft-fullcone
-git clone https://github.com/chenmozhijin/turboacc
-mv ./turboacc/luci-app-turboacc ./luci-app-turboacc
-rm -rf ./turboacc
-cd ../..
-cp -f turboacc_tmp/turboacc/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch
-cp -f turboacc_tmp/turboacc/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
-cp -f turboacc_tmp/turboacc/pending-5.15/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-5.15/613-netfilter_optional_tcp_window_check.patch
-rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
-mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
-cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
-cp -RT ./turboacc_tmp/turboacc/firewall4-$(grep -o 'FIREWALL4_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/firewall4 ./package/network/config/firewall4
-cp -RT ./turboacc_tmp/turboacc/libnftnl-$(grep -o 'LIBNFTNL_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/libnftnl ./package/libs/libnftnl
-cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/nftables ./package/network/utils/nftables
-rm -rf turboacc_tmp
-echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-5.15
-echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-5.15
-sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh-cn/turboacc.po
-echo "install turboacc done!"
+#echo "install turboacc and patch for nftables"
+#ls
+#mkdir -p turboacc_tmp ./package/turboacc
+#cd turboacc_tmp
+#git clone https://github.com/chenmozhijin/turboacc -b package
+#cd ../package/turboacc
+#git clone https://github.com/fullcone-nat-nftables/nft-fullcone
+#git clone https://github.com/chenmozhijin/turboacc
+#mv ./turboacc/luci-app-turboacc ./luci-app-turboacc
+#rm -rf ./turboacc
+#cd ../..
+#cp -f turboacc_tmp/turboacc/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-5.15/952-add-net-conntrack-events-support-multiple-registrant.patch
+#cp -f turboacc_tmp/turboacc/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-5.15/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+#cp -f turboacc_tmp/turboacc/pending-5.15/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-5.15/613-netfilter_optional_tcp_window_check.patch
+#rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
+#mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
+#cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
+#cp -RT ./turboacc_tmp/turboacc/firewall4-$(grep -o 'FIREWALL4_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/firewall4 ./package/network/config/firewall4
+#cp -RT ./turboacc_tmp/turboacc/libnftnl-$(grep -o 'LIBNFTNL_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/libnftnl ./package/libs/libnftnl
+#cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/nftables ./package/network/utils/nftables
+#rm -rf turboacc_tmp
+#echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-5.15
+#echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-5.15
+#sed -i 's/Turbo ACC 网络加速/网络加速/g' package/turboacc/luci-app-turboacc/po/zh-cn/turboacc.po
+#echo "install turboacc done!"
 
 # Boost UDP
 echo '# optimize udp' >>package/base-files/files/etc/sysctl.d/10-default.conf
@@ -72,5 +72,9 @@ wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64
 
 # upgrade config
 wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/99-init-settings -O package/base-files/files/etc/uci-defaults/99-init-settings
+
+# add dns list
+mkdir -p package/base-files/files/etc/dnsmasq.d
+wget --no-check-certificate -O package/base-files/files/etc/dnsmasq.d/accelerated-domains.china.conf "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
 
 echo "diy-part2.sh is done."
