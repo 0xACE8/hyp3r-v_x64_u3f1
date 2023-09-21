519c2ebf4519a1c574029e246f54401b54d690de
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2023-09-20 18:15:22 -0700

    Update d1y-p4r72.sh

--- d1y-p4r72.sh
+++ d1y-p4r72.sh
@@ -7,9 +7,6 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.177.80/g' package/base-files/files/bin/config_generate
 
-# Password is ********
-#sed -i "s/root::0:0:99999:7:::/root:$1$4xKZB45Q$w0CPT5M6vBWbYNmSWuxfU.:19007:0:99999:7:::/g" package/base-files/files/etc/shadow
-
 # Change language=auto to zh_cn
 sed -i 's/lang="auto"/lang="zh_cn"/g' package/emortal/default-settings/files/99-default-settings
 
@@ -19,16 +16,26 @@ sed -i 's/lang="auto"/lang="zh_cn"/g' package/emortal/default-settings/files/99-
 #sed -i 's/ntp.tencent.com/edu.ntp.org.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 #sed -i 's/ntp.tencent.com/ntp.tuna.tsinghua.edu.cn/g' package/emortal/default-settings/files/99-default-settings-chinese
 
-# Change luci list name
+# luci theme argon update
 sed -i 's/"Argon 主题设置"/"主题设置"/g' feeds/luci/applications/luci-app-argon-config/po/zh_Hans/argon-config.po
-
-# change some tips
-sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
-
-sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
 
 rm -rf feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img
 wget --no-check-certificate -O feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img "https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/bg1.jpg"
+
+sed -i 's/"#5e72e4"/"#bc1142"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+sed -i 's/"#5e72e4"/"#bc1142"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
+sed -i 's/"#483d8b"/"#bc1142"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
+sed -i 's/"#483d8b"/"#bc1142"/g' feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/css/dark.css
+
+# ttyd menu name
+sed -i 's/"终端"/"TTYD 终端"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
+
+# dnscrypt-proxy start
+sed -i 's/START=18/START=99/g' feeds/packages/net/dnscrypt-proxy2/files/dnscrypt-proxy.init
+
+# DHCP
+mkdir -p package/base-files/files/etc/dnsmasq.d
+wget --no-check-certificate -O package/base-files/files/etc/dnsmasq.d/accelerated-domains.china.conf "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
 
 # install turboacc and patch
 #echo "install turboacc and patch for nftables"
@@ -65,19 +72,15 @@ echo 'net.core.wmem_max=26214400' >>package/base-files/files/etc/sysctl.d/10-def
 echo 'net.core.wmem_default=26214400' >>package/base-files/files/etc/sysctl.d/10-default.conf
 echo 'net.core.netdev_max_backlog=2048' >>package/base-files/files/etc/sysctl.d/10-default.conf
 
-# Change to my banner
-sudo rm package/base-files/files/etc/banner
-wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/banner -O package/base-files/files/etc/banner
-
 # upgrade 99-default-settings-chinese.sh
 rm -rf package/emortal/default-settings/files/99-default-settings-chinese.sh
 wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/99-default-settings-chinese.sh -O package/emortal/default-settings/files/99-default-settings-chinese.sh
 
-# upgrade config
+# add init settings
 wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/99-init-settings -O package/base-files/files/etc/uci-defaults/99-init-settings
 
-# add dns list
-mkdir -p package/base-files/files/etc/dnsmasq.d
-wget --no-check-certificate -O package/base-files/files/etc/dnsmasq.d/accelerated-domains.china.conf "https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+# Change to my banner
+sudo rm package/base-files/files/etc/banner
+wget --no-check-certificate https://raw.githubusercontent.com/0xACE8/hyp3r-v_x64_u3f1/main/banner -O package/base-files/files/etc/banner
 
 echo "diy-part2.sh is done."
