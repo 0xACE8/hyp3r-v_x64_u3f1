9d41118a9fd47702f786587c5be0c2abab0fd4f5
0xACE8 <19428059+0xACE8@users.noreply.github.com>
2024-03-10 01:32:55 -0800

    Update d1y-p4r71.sh

--- d1y-p4r71.sh
+++ d1y-p4r71.sh
@@ -2,7 +2,4 @@ sed -i '1 i\src-git ace8 https://github.com/0xACE8/4c38-p4ck463;main' feeds.conf
 sed -i '2 i\src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
 sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' feeds.conf.default
 #sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns;v5' feeds.conf.default
-sed -i '$a src-git mosdns https://github.com/sbwml/luci-app-mosdns/tree/21bff11aebfdb0b0f8fdab8c4b1322e404b7038c' feeds.conf.default
-
-
-
+sed -i '$a src-git mosdns https://github.com/0xACE8/luci-app-mosdns' feeds.conf.default
